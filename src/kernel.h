27993e21fb69807c91f46a4d40f0d9525c29af0f
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2020-11-08 18:12:17 +0000

    update less

---
+++ src/kernel.h
@@ -0,0 +1,14 @@
+// Copyright (c) 2012-2013 The PPCoin developers
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef PPCOIN_KERNEL_H
+#define PPCOIN_KERNEL_H
+
+#include "main.h"
+
+// MODIFIER_INTERVAL: time to elapse before new modifier is computed
+extern unsigned int nModifierInterval;
+
+// MODIFIER_INTERVAL_RATIO:
+// ratio of group interval length between the last group and the first group
+static const int MODIFIER_INTER
