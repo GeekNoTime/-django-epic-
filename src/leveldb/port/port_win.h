b02dae3be5dddef01c6587296a0fd3299b9b7367
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-01-09 21:29:30 +0000

    login

---
+++ src/leveldb/port/port_win.h
@@ -0,0 +1,14 @@
+// LevelDB Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+//
+// See port_example.h for documentation for the following types/functions.
+
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+// 
+//  * Redistributions of source code must retain the above copyright
+//    notice, this list of conditions and the following disclaimer.
+//  * Redistributions in binary form must reproduce the above copyright
+//    notice, this list of conditions and the following disclaimer in the
+//    documentatio
