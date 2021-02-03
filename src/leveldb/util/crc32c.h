8f2ce67af0a7c825ff0794dc2d8ed8de07bad1a4
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-02-03 08:52:15 +0000

    Remove debugging line.

---
+++ src/leveldb/util/crc32c.h
@@ -0,0 +1,16 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_UTIL_CRC32C_H_
+#define STORAGE_LEVELDB_UTIL_CRC32C_H_
+
+#include <stddef.h>
+#include <stdint.h>
+
+namespace leveldb {
+namespace crc32c {
+
+// Return the crc32c of concat(A, data[0,n-1]) where init_crc is the
+// crc32c of some string A.  Extend() is often used to maintain the
+// crc32c of a stre
