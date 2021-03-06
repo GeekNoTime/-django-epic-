7ca3b3789a59f12add6f6648b6d575907e4d9c16
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-03-06 14:51:53 +0000

    Refactoring namespace

---
+++ src/leveldb/util/testharness.h
@@ -0,0 +1,9 @@
+// Copyright (c) 2011 The LevelDB Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file. See the AUTHORS file for names of contributors.
+
+#ifndef STORAGE_LEVELDB_UTIL_TESTHARNESS_H_
+#define STORAGE_LEVELDB_UTIL_TESTHARNESS_H_
+
+#include <stdio.h>
+#include <s
