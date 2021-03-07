0f4cbcd65d359e8901a4b80024cc579cb7a550af
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-03-07 16:05:07 +0000

    filtros

--- src/main.h
+++ src/main.h
@@ -1,3 +1,80 @@
 // Copyright (c) 2009-2010 Satoshi Nakamoto
 // Copyright (c) 2009-2012 The Bitcoin developers
-//
+// Distributed under the MIT/X11 software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#ifndef BITCOIN_MAIN_H
+#define BITCOIN_MAIN_H
+
+#include "bignum.h"
+#include "sync.h"
+#include "net.h"
+#include "script.h"
+#include "scrypt.h"
+#include "zerocoin/Zerocoin.h"
+
+#include <list>
+
+class CWallet;
+class CBlock;
+class CBlockIndex;
+class CKeyItem;
+class CReserveKey;
+class COutPoint;
+
+class CAddress;
+class CInv;
+class CRequestTracker;
+class CNode;
+
+static const int LAST_POW_BLOCK = 10800;
+
+static const unsigned int MAX_BLOCK_SIZE = 1000000;
+static const unsigned int MAX_BLOCK_SIZE_GEN = MAX_BLOCK_SIZE/2;
+static const unsigned int MAX_BLOCK_SIGOPS = MAX_BLOCK_SIZE/50;
+static const unsigned int MAX_ORPHAN_TRANSACTIONS = MAX_BLOCK_SIZE/100;
+static const unsigned int MAX_INV_SZ = 50000;
+static const int64_t MIN_TX_FEE = 10000;
+static const int64_t MIN_RELAY_TX_FEE = MIN_TX_FEE;
+static const int64_t MAX_MONEY = 210000000 * COIN;
+static const int64_t COIN_YEAR_REWARD = 3.65 * CENT; // 1% per year
+static const int64_t MAX_MINT_PROOF_OF_STAKE = 0.0365 * COIN;	// 3.65% annual interest
+
+inline bool MoneyRange(int64_t nValue) { return (nValue >= 0 && nValue <= MAX_MONEY); }
+// Threshold for nLockTime: below this value it is interpreted as block number, otherwise as UNIX timestamp.
+static const unsigned int LOCKTIME_THRESHOLD = 500000000; // Tue Nov  5 00:53:20 1985 UTC
+
+#ifdef USE_UPNP
+static const int fHaveUPnP = true;
+#else
+static const int fHaveUPnP = false;
+#endif
+
+static const uint256 hashGenesisBlock("0xf05efb1b56c722bff45d512dbdfdd13c9ce1d37aff9e503ec18344329e41cc3f");
+static const uint256 hashGenesisBlockTestNet("0xd1b1e2b53ff40a5cd973556ddff08d209b9573b69722e126b90bc55246c69c82");
+
+inline int64_t PastDrift(int64_t nTime)   { return nTime - 10 * 60; } // up to 10 minutes from the past
+inline int64_t FutureDrift(int64_t nTime) { return nTime + 10 * 60; } // up to 10 minutes from the future
+
+extern libzerocoin::Params* ZCParams;
+extern CScript COINBASE_FLAGS;
+extern CCriticalSection cs_main;
+extern std::map<uint256, CBlockIndex*> mapBlockIndex;
+extern std::set<std::pair<COutPoint, unsigned int> > setStakeSeen;
+extern CBlockIndex* pindexGenesisBlock;
+extern unsigned int nStakeMinAge;
+extern unsigned int nNodeLifespan;
+extern int nCoinbaseMaturity;
+extern int nBestHeight;
+extern uint256 nBestChainTrust;
+extern uint256 nBestInvalidTrust;
+extern uint256 hashBestChain;
+extern CBlockIndex* pindexBest;
+extern unsigned int nTransactionsUpdated;
+extern uint64_t nLastBlockTx;
+extern uint64_t nLastBlockSize;
+extern int64_t nLastCoinStakeSearchInterval;
+extern const std::string strMessageMagic;
+extern int64_t nTimeBestReceived;
+extern CCriticalSection cs_setpwalletRegistered;
+extern std::set<CWallet*> setpwalletRegistered;
+extern unsigned char pchMes
