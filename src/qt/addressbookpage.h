2ea780d17406ac28641259e066c4d3f7a6d72671
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-06-12 11:31:49 +0000

    draft notes

---
+++ src/qt/addressbookpage.h
@@ -0,0 +1,53 @@
+#ifndef ADDRESSBOOKPAGE_H
+#define ADDRESSBOOKPAGE_H
+
+#include <QDialog>
+
+namespace Ui {
+    class AddressBookPage;
+}
+class AddressTableModel;
+class OptionsModel;
+
+QT_BEGIN_NAMESPACE
+class QTableView;
+class QItemSelection;
+class QSortFilterProxyModel;
+class QMenu;
+class QModelIndex;
+QT_END_NAMESPACE
+
+/** Widget that shows a list of sending or receiving addresses.
+  */
+class AddressBookPage : public QDialog
+{
+    Q_OBJECT
+
+public:
+    enum Tabs {
+        SendingTab = 0,
+        ReceivingTab = 1
+    };
+
+    enum Mode {
+        ForSending, /**< Open address book to pick address for sending */
+        ForEditing  /**< Open address book for editing */
+    };
+
+    explicit AddressBookPage(Mode mode, Tabs tab, QWidget *parent = 0);
+    ~AddressBookPage();
+
+    void setModel(AddressTableModel *model);
+    void setOptionsModel(OptionsModel *optionsModel);
+    const QString &getReturnValue() const { return returnValue; }
+
+public slots:
+    void done(int retval);
+    void exportClicked();
+
+private:
+    Ui::AddressBookPage *ui;
+    AddressTableModel *model;
+    OptionsModel *optionsModel;
+    Mode mode;
+    T
