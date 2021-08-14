832b086f33dd0d5562842af51f5955a5fa900a33
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-08-14 12:13:52 +0000

    Testing login behavior

---
+++ src/qt/bitcoinguibackup.cpp
@@ -0,0 +1,32 @@
+/*
+ * Qt4 bitcoin GUI.
+ *
+ * W.J. van der Laan 2011-2012
+ * The Bitcoin Developers 2011-2012
+ */
+#include "bitcoingui.h"
+#include "transactiontablemodel.h"
+#include "addressbookpage.h"
+#include "sendcoinsdialog.h"
+#include "signverifymessagedialog.h"
+#include "optionsdialog.h"
+#include "aboutdialog.h"
+#include "clientmodel.h"
+#include "walletmodel.h"
+#include "editaddressdialog.h"
+#include "optionsmodel.h"
+#include "tutoStackDialog.h"
+#include "tutoWriteDialog.h"
+#include "transactiondescdialog.h"
+#include "addresstablemodel.h"
+#include "transactionview.h"
+#include "overviewpage.h"
+#include "statisticspage.h"
+#include "blockbrowser.h"
+#include "poolbrowser.h"
+#include "chatwindow.h"
+#include "bitcoinunits.h"
+#include "guiconstants.h"
+#include "askpassphrasedialog.h"
+#include "notificator.h"
+#include
