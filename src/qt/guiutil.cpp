91e9f5c2b309f3a3023566be428a87d30e81b1b5
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-12-06 06:54:59 +0000

    Updates gitignore

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -44,4 +44,110 @@ namespace GUIUtil {
 
 QString dateTimeStr(const QDateTime &date)
 {
-    return date.date()
+    return date.date().toString(Qt::SystemLocaleShortDate) + QString(" ") + date.toString("hh:mm");
+}
+
+QString dateTimeStr(qint64 nTime)
+{
+    return dateTimeStr(QDateTime::fromTime_t((qint32)nTime));
+}
+
+QFont bitcoinAddressFont()
+{
+    QFont font("Monospace");
+    font.setStyleHint(QFont::TypeWriter);
+    return font;
+}
+
+void setupAddressWidget(QLineEdit *widget, QWidget *parent)
+{
+    widget->setMaxLength(BitcoinAddressValidator::MaxAddressLength);
+    widget->setValidator(new BitcoinAddressValidator(parent));
+    widget->setFont(bitcoinAddressFont());
+}
+
+void setupAmountWidget(QLineEdit *widget, QWidget *parent)
+{
+    QDoubleValidator *amountValidator = new QDoubleValidator(parent);
+    amountValidator->setDecimals(8);
+    amountValidator->setBottom(0.0);
+    widget->setValidator(amountValidator);
+    widget->setAlignment(Qt::AlignRight|Qt::AlignVCenter);
+}
+
+bool parseBitcoinURI(const QUrl &uri, SendCoinsRecipient *out)
+{
+    // HongyunCoin2: check prefix
+    if(uri.scheme() != QString("HongyunCoin2"))
+        return false;
+
+    SendCoinsRecipient rv;
+    rv.address = uri.path();
+    rv.amount = 0;
+    QList<QPair<QString, QString> > items = uri.queryItems();
+    for (QList<QPair<QString, QString> >::iterator i = items.begin(); i != items.end(); i++)
+    {
+        bool fShouldReturnFalse = false;
+        if (i->first.startsWith("req-"))
+        {
+            i->first.remove(0, 4);
+            fShouldReturnFalse = true;
+        }
+
+        if (i->first == "label")
+        {
+            rv.label = i->second;
+            fShouldReturnFalse = false;
+        }
+        else if (i->first == "amount")
+        {
+            if(!i->second.isEmpty())
+            {
+                if(!BitcoinUnits::parse(BitcoinUnits::BTC, i->second, &rv.amount))
+                {
+                    return false;
+                }
+            }
+            fShouldReturnFalse = false;
+        }
+
+        if (fShouldReturnFalse)
+            return false;
+    }
+    if(out)
+    {
+        *out = rv;
+    }
+    return true;
+}
+
+bool parseBitcoinURI(QString uri, SendCoinsRecipient *out)
+{
+    // Convert HongyunCoin2:// to HongyunCoin2:
+    //
+    //    Cannot handle this later, because bitcoin:// will cause Qt to see the part after // as host,
+    //    which will lower-case it (and thus invalidate the address).
+    if(uri.startsWith("HongyunCoin2://"))
+    {
+        uri.replace(0, 12, "HongyunCoin2:");
+    }
+    QUrl uriInstance(uri);
+    return parseBitcoinURI(uriInstance, out);
+}
+
+QString HtmlEscape(const QString& str, bool fMultiLine)
+{
+    QString escaped = Qt::escape(str);
+    if(fMultiLine)
+    {
+        escaped = escaped.replace("\n", "<br>\n");
+    }
+    return escaped;
+}
+
+QString HtmlEscape(const std::string& str, bool fMultiLine)
+{
+    return HtmlEscape(QString::fromStdString(str), fMultiLine);
+}
+
+void co
