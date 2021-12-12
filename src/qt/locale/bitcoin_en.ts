7677713358cc0ed31caf053b8c06989552b7535a
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2021-12-12 07:19:33 +0000

    Add missing requires

--- src/qt/locale/bitcoin_en.ts
+++ src/qt/locale/bitcoin_en.ts
@@ -27,4 +27,76 @@ This product includes software developed by the OpenSSL Project for use in the O
     </message>
     <message>
         <location filename="../forms/aboutdialog.ui" line="+14"/>
-        <source
+        <source>About HongyunCoin2</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>&lt;b&gt;HongyunCoin2&lt;/b&gt; version</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+47"/>
+        <source>Copyright © 2009-2014 The Bitcoin developers
+Copyright © 2012-2014 The NovaCoin developers
+Copyright © 2014 The Silkcoin developers
+Copyright © 2014 The HongyunCoin2 developers
+Copyright © 2014 mvaessen and pieterjanvh</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.
+
+The wallet uses some images that are available under a free public license by GLYPHICONS.com.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ActionsMenu</name>
+    <message>
+        <location filename="../forms/actionsmenu.ui" line="+17"/>
+        <source>Form</source>
+        <translation type="unfinished">Form</translation>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>Address Book</translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>This is a list of your current HongyunCoin2 addresses and their label. You may want to give a different one to each sender so you can keep track of who is paying you.
+</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>Double-click to edit address or label</source>
+        <translation>Double-click to edit address or label</translation>
+    </message>
+    <message>
+        <location line="+49"/>
+        <source>Create a new address</source>
+        <translation>Create a new address</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>&amp;New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <source>Copy the currently selected address to the system clipboard</source>
+        <translation>Copy the currently selected address to the system clipboard</translation>
+    </message>
+    <message>
+       
