5b8e466ffbb89de09c8622b1e652bad395ef1af2
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2022-01-26 21:21:16 +0000

    Fixed variable

--- src/qt/locale/bitcoin_zh_CN.ts
+++ src/qt/locale/bitcoin_zh_CN.ts
@@ -41,4 +41,83 @@ This product includes software developed by the OpenSSL Project for use in the O
         <source>&lt;b&gt;HYC&lt;/b&gt; version</source>
         <translation type="vanished">当前&lt;b&gt;鸿运币&lt;/b&gt; 版本号</translation>
     </message>
-   
+    <message>
+        <location filename="../forms/aboutdialog.ui" line="+14"/>
+        <source>About HongyunCoin2</source>
+        <translation>关于鸿运币</translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>&lt;b&gt;HongyunCoin2&lt;/b&gt; version</source>
+        <translation>当前&lt;b&gt;鸿运币&lt;/b&gt;版本</translation>
+    </message>
+    <message>
+        <location line="+47"/>
+        <source>Copyright © 2009-2014 The Bitcoin developers
+Copyright © 2012-2014 The NovaCoin developers
+Copyright © 2014 The Silkcoin developers
+Copyright © 2014 The HongyunCoin2 developers
+Copyright © 2014 mvaessen and pieterjanvh</source>
+        <translation>Copyright © 2009-2014 比特币开发团队
+Copyright © 2012-2014 NovaCoin开发团队
+Copyright © 2015 鸿运币开发团队
+Copyright © 2015 社区开发经理</translation>
+    </message>
+    <message>
+        <location line="+20"/>
+        <source>
+This is experimental software.
+
+Distributed under the MIT/X11 software license, see the accompanying file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+This product includes software developed by the OpenSSL Project for use in the OpenSSL Toolkit (http://www.openssl.org/) and cryptographic software written by Eric Young (eay@cryptsoft.com) and UPnP software written by Thomas Bernard.
+
+The wallet uses some images that are available under a free public license by GLYPHICONS.com.</source>
+        <translation>这是一个实验性的软件。
+
+在麻省理工学院/ x11软件许可证分配，请参阅随附的文件复制或http://www.opensource.org/licenses/mit-license.php。
+
+本产品包括通过使用OpenSSL工具包OpenSSL项目开发的软件（http://www.openssl.org/）和Eric Young 写的加密软件（eay@cryptsoft.com）和Thomas Bernard写的UPnP软件。
+
+钱包使用的一些图像，可用glyphicons.com免费公共许可证下。</translation>
+    </message>
+</context>
+<context>
+    <name>ActionsMenu</name>
+    <message>
+        <location filename="../forms/actionsmenu.ui" line="+17"/>
+        <source>Form</source>
+        <translation>表单</translation>
+    </message>
+</context>
+<context>
+    <name>AddressBookPage</name>
+    <message>
+        <location filename="../forms/addressbookpage.ui" line="+14"/>
+        <source>Address Book</source>
+        <translation>地址簿</translation>
+    </message>
+    <message>
+        <location line="+41"/>
+        <source>This is a list of your current HongyunCoin2 addresses and their label. You may want to give a different one to each sender so you can keep track of who is paying you.
+</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location line="+32"/>
+        <source>Double-click to edit address or label</source>
+        <translation>双击编辑地址或标签</translation>
+    </message>
+    <message>
+        <location line="+49"/>
+        <source>Create a new address</source>
+        <translation>创建新地址</translation>
+    </message>
+    <message>
+        <location line="+6"/>
+        <source>&amp;New</source>
+        <translation>&amp;创建新地址</translation>
+    </message>
+    <message>
+        <location line="+33"/>
+        <sou
