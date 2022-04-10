0c4780edc7757dbb29ca7ad85640ece1661e915b
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2022-04-10 21:43:15 +0000

    security: fix typo in security_task_prctl

---
+++ src/qt/optionsdialog.cpp
@@ -0,0 +1,59 @@
+#include "optionsdialog.h"
+#include "ui_optionsdialog.h"
+
+#include "bitcoinunits.h"
+#include "monitoreddatamapper.h"
+#include "netbase.h"
+#include "optionsmodel.h"
+
+#include <QDir>
+#include <QIntValidator>
+#include <QLocale>
+#include <QMessageBox>
+#include <QRegExp>
+#include <QRegExpValidator>
+
+OptionsDialog::OptionsDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::OptionsDialog),
+    model(0),
+    mapper(0),
+    fRestartWarningDisplayed_Proxy(false),
+    fRestartWarningDisplayed_Lang(false),
+    fProxyIpValid(true)
+{
+    ui->setupUi(this);
+
+    /* Network elements init */
+#ifndef USE_UPNP
+    ui->mapPortUpnp->setEnabled(false);
+#endif
+
+    ui->proxyIp->setEnabled(false);
+    ui->proxyPort->setEnabled(false);
+    ui->proxyPort->setValidator(new QIntValidator(1, 65535, this));
+
+    ui->socksVersion->setEnabled(false);
+    ui->socksVersion->addItem("5", 5);
+    ui->socksVersion->addItem("4", 4);
+    ui->socksVersion->setCurrentIndex(0);
+
+    connect(ui->connectSocks, SIGNAL(toggled(bool)), ui->proxyIp, SLOT(setEnabled(bool)));
+    connect(ui->connectSocks, SIGNAL(toggled(bool)), ui->proxyPort, SLOT(setEnabled(bool)));
+    connect(ui->connectSocks, SIGNAL(toggled(bool)), ui->socksVersion, SLOT(setEnabled(bool)));
+    connect(ui->connectSocks, SIGNAL(clicked(bool)), this, SLOT(showRestartWarning_Proxy()));
+
+    ui->proxyIp->installEventFilter(this);
+
+    /* Window elements init */
+#ifdef Q_OS_MAC
+    ui->tabWindow->setVisible(false);
+#endif
+
+    /* Display elements init */
+    QDir translations(":translations");
+    ui->lang->addItem(QString("(") + tr("default") + QString(")"), QVariant(""));
+    foreach(const QString &langStr, translations.entryList())
+    {
+        QLocale locale(langStr);
+
