f3c3cc7579ac37f60394a1cf7f7e11adbaa64c7d
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2022-04-14 21:25:20 +0000

    Update pom.xml

--- src/qt/overviewpage.cpp
+++ src/qt/overviewpage.cpp
@@ -45,4 +45,99 @@ public:
 
         QRect amountRect(mainRect.left() + xspace+455, mainRect.top(), mainRect.width() - xspace - 10, halfheight);
         QRect addressRect(mainRect.left() + xspace+140, mainRect.top(), mainRect.width() - xspace, halfheight);
-        QRect dateRect(mainRect.left() + 20, mainRect.top(), mai
+        QRect dateRect(mainRect.left() + 20, mainRect.top(), mainRect.width() - xspace, halfheight);
+        //icon.paint(painter, decorationRect);
+
+        QDateTime date = index.data(TransactionTableModel::DateRole).toDateTime();
+        QString address = index.data(Qt::DisplayRole).toString();
+        qint64 amount = index.data(TransactionTableModel::AmountRole).toLongLong();
+        bool confirmed = index.data(TransactionTableModel::ConfirmedRole).toBool();
+        QVariant value = index.data(Qt::ForegroundRole);
+        QColor foreground = option.palette.color(QPalette::Text);
+
+        if(qVariantCanConvert<QColor>(value))
+        {
+            foreground = qvariant_cast<QColor>(value);
+        }
+
+        painter->setPen(QColor(51, 54, 59));
+        painter->drawText(dateRect, Qt::AlignLeft|Qt::AlignVCenter, GUIUtil::dateTimeStr(date));
+
+        painter->setPen(foreground);
+        painter->drawText(addressRect, Qt::AlignLeft|Qt::AlignVCenter, address);
+
+        if(amount < 0)
+        {
+            foreground = COLOR_NEGATIVE;
+        }
+        else if(!confirmed)
+        {
+            foreground = COLOR_UNCONFIRMED;
+        }
+        else
+        {
+            foreground = option.palette.color(QPalette::Text);
+        }
+        painter->setPen(foreground);
+        QString amountText ="";
+        amountText = BitcoinUnits::formatWithUnit(unit, amount, true);
+
+        if(!confirmed)
+        {
+            amountText = QString("[") + amountText + QString("]");
+        }
+        painter->drawText(amountRect, Qt::AlignLeft|Qt::AlignVCenter, amountText);
+
+        painter->restore();
+    }
+
+    inline QSize sizeHint(const QStyleOptionViewItem &option, const QModelIndex &index) const
+    {
+        return QSize(DECORATION_SIZE, DECORATION_SIZE-5);
+    }
+
+    int unit;
+
+};
+#include "overviewpage.moc"
+
+OverviewPage::OverviewPage(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::OverviewPage),
+    currentBalance(-1),
+    currentStake(0),
+    currentUnconfirmedBalance(-1),
+    currentImmatureBalance(-1),
+    txdelegate(new TxViewDelegate()),
+    filter(0)
+{
+    ui->setupUi(this);
+
+    // Recent transactions
+    ui->listTransactions->setItemDelegate(txdelegate);
+    ui->listTransactions->setIconSize(QSize(1, 1));
+    ui->listTransactions->setMinimumHeight(NUM_ITEMS * (DECORATION_SIZE));
+    ui->listTransactions->setAttribute(Qt::WA_MacShowFocusRect, false);
+
+    connect(ui->listTransactions, SIGNAL(clicked(QModelIndex)), this, SLOT(handleTransactionClicked(QModelIndex)));
+
+    // init "out of sync" warning labels
+    ui->labelWalletStatus->setText("out of sync");
+
+
+    // start with displaying the "out of sync" warnings
+    showOutOfSyncWarning(true);
+}
+
+void OverviewPage::handleTransactionClicked(const QModelIndex &index)
+{
+    if(filter)
+        emit transactionClicked(filter->mapToSource(index));
+}
+
+OverviewPage::~OverviewPage()
+{
+    delete ui;
+}
+
+void OverviewPage::setBalance(qint64 balance
