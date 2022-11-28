0fac79aa4a11705a2cdbed4e3b12f203b28d436b
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2022-11-28 12:57:01 +0000

    jdk

--- src/txdb-leveldb.h
+++ src/txdb-leveldb.h
@@ -75,4 +75,108 @@ protected:
             }
         }
         if (readFromDb) {
-            leveldb::Status status = 
+            leveldb::Status status = pdb->Get(leveldb::ReadOptions(),
+                                              ssKey.str(), &strValue);
+            if (!status.ok()) {
+                if (status.IsNotFound())
+                    return false;
+                // Some unexpected error.
+                printf("LevelDB read failure: %s\n", status.ToString().c_str());
+                return false;
+            }
+        }
+        // Unserialize value
+        try {
+            CDataStream ssValue(strValue.data(), strValue.data() + strValue.size(),
+                                SER_DISK, CLIENT_VERSION);
+            ssValue >> value;
+        }
+        catch (std::exception &e) {
+            return false;
+        }
+        return true;
+    }
+
+    template<typename K, typename T>
+    bool Write(const K& key, const T& value)
+    {
+        if (fReadOnly)
+            assert(!"Write called on database in read-only mode");
+
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        ssKey.reserve(1000);
+        ssKey << key;
+        CDataStream ssValue(SER_DISK, CLIENT_VERSION);
+        ssValue.reserve(10000);
+        ssValue << value;
+
+        if (activeBatch) {
+            activeBatch->Put(ssKey.str(), ssValue.str());
+            return true;
+        }
+        leveldb::Status status = pdb->Put(leveldb::WriteOptions(), ssKey.str(), ssValue.str());
+        if (!status.ok()) {
+            printf("LevelDB write failure: %s\n", status.ToString().c_str());
+            return false;
+        }
+        return true;
+    }
+
+    template<typename K>
+    bool Erase(const K& key)
+    {
+        if (!pdb)
+            return false;
+        if (fReadOnly)
+            assert(!"Erase called on database in read-only mode");
+
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        ssKey.reserve(1000);
+        ssKey << key;
+        if (activeBatch) {
+            activeBatch->Delete(ssKey.str());
+            return true;
+        }
+        leveldb::Status status = pdb->Delete(leveldb::WriteOptions(), ssKey.str());
+        return (status.ok() || status.IsNotFound());
+    }
+
+    template<typename K>
+    bool Exists(const K& key)
+    {
+        CDataStream ssKey(SER_DISK, CLIENT_VERSION);
+        ssKey.reserve(1000);
+        ssKey << key;
+        std::string unused;
+
+        if (activeBatch) {
+            bool deleted;
+            if (ScanBatch(ssKey, &unused, &deleted) && !deleted) {
+                return true;
+            }
+        }
+
+
+        leveldb::Status status = pdb->Get(leveldb::ReadOptions(), ssKey.str(), &unused);
+        return status.IsNotFound() == false;
+    }
+
+
+public:
+    bool TxnBegin();
+    bool TxnCommit();
+    bool TxnAbort()
+    {
+        delete activeBatch;
+        activeBatch = NULL;
+        return true;
+    }
+
+    bool ReadVersion(int& nVersion)
+    {
+        nVersion = 0;
+        return Read(std::string("version"), nVersion);
+    }
+
+    bool WriteVersion(int nVersion)
+ 
