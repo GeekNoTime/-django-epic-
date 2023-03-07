4e6eb829545418525464d8219f5bcc62f7aec9b7
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2023-03-07 13:32:48 +0000

    Sanitize

---
+++ src/zerocoin/SerialNumberSignatureOfKnowledge.cpp
@@ -0,0 +1,6 @@
+/**
+* @file       SerialNumberSignatureOfKnowledge.cpp
+*
+* @brief      SerialNumberSignatureOfKnowledge class for the Zerocoin library.
+*
+* @author     Ian Miers, Christina Garman an
