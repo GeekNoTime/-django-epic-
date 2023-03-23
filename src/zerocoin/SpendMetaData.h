c03ddfc29ba1b8e2d7ea381d2d3f31adcc51496e
GeekNoTime <107592759+GeekNoTime@users.noreply.github.com>
2023-03-23 15:17:59 +0000

    chore(release): Prepare next development version: 7.6.0-SNAPSHOT

--- src/zerocoin/SpendMetaData.h
+++ src/zerocoin/SpendMetaData.h
@@ -27,4 +27,26 @@ public:
 	/**
 	 * Creates meta data associated with a coin spend
 	 * @param accumulatorId hash of block containing accumulator
-	 * @param txHash hash of tra
+	 * @param txHash hash of transaction
+	 */
+	SpendMetaData(uint256 accumulatorId, uint256 txHash);
+
+	/**
+	 * The hash of the block containing the accumulator CoinSpend
+	 * proves membership in.
+	 */
+	uint256 accumulatorId; // The block the accumulator is in
+	/**Contains the hash of the rest of transaction
+	 * spending a zerocoin (excluding the coinspend proof)
+	 */
+	uint256 txHash; // The Hash of the rest of the transaction the spend proof is n.
+	// Allows us to sign the transaction.
+	IMPLEMENT_SERIALIZE
+	(
+	    READWRITE(accumulatorId);
+	    READWRITE(txHash);
+	)
+};
+
+} /* namespace libzerocoin */
+#endif /* SPENDMETADATA_H_ */
---
+++ src/zerocoin/ZeroTest.h
@@ -0,0 +1,7 @@
+
+#ifndef ZEROTEST_H_
+#define ZEROTEST_H_
+
+void Test_RunAllTests();
+
+#endif
